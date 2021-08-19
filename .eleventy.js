e54417cb9930ace52a4cb15174169512aa2e0252
Paul Doherty <paul@pauldoherty.dev>
2021-08-19 21:09:13 +0100

    removed jquery

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,8 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy("./src/img");
     eleventyConfig.addWatchTarget("./src/img/");
+    eleventyConfig.addPassthroughCopy("./src/js");
+    eleventyConfig.addWatchTarget("./src/js/");
 
     return {
         dir: {
---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+    /* Toggle menu */
+    const menuToggleBtn = document.querySelector('.icon-menu');
+    const menuContainer = document.querySelector('.aside-main');
+
+    menuToggleBtn.addEventListener('click', () => {
+        menuContainer.classList.toggle('active');
+    })
+
+    /* Back to top */
+    const scrollBtn = document.querySelector('.back-to-top');
+
+    function scrollToTop() {
+        window.scroll({top: 0, left: 0, behavior: 'smooth'});
+    }
+
+    scrollBtn.addEventListener('click', () => {
+        scrollToTop();
+    })
+
+    /* Toggle projects */
+
+    const projectToggles = document.querySelectorAll('.project-show');
+
+    projectToggles.forEach(toggle => {
+        toggle.addEventListener('click', (e)=>{
+            e.preventDefault();
+            let newText = (toggle.innerText === 'Show Projects')
+                ? 'Hide Projects'
+                : 'Show Projects'
+            toggle.innerText = newText;
+            let projectDetails = document.getElementById(toggle.dataset.projects);
+            projectDetails.classList.toggle('d-none')
+        })
+    })
